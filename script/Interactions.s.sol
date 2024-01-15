0f712730bd6b1cdbee7cf7f6c26dc2f61f91e791
worksofallwin <princeallwin199@gmail.com>
2024-01-15 18:57:35 +0530

    update uri in interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
     string constant PUG_TOKEN_URI = "ipfs://bafybeietajviz34cqtxjuv74orhpakbi2th5qok5qscym4a5srskverxua/";
-    // string constant PUG_TOKEN_URI =
-    //     "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function mintNftOnContract(address basicNft) public {
         vm.startBroadcast();
